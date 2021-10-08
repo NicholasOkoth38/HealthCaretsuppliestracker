99f750a6b55e4af75b7317bc62885696ec31de9c
Koech-code <nixon.koech@moringaschool.com>
2021-10-09 00:20:55 +0300

    add the app name and rest_framework in the installed apps

--- HealthCaretsuppliestracker/settings.py
+++ HealthCaretsuppliestracker/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+AUTH_USER_MODEL='healthCaretsuppliestracker.User'
 # Application definition
 
 INSTALLED_APPS = [
@@ -77,11 +77,12 @@ WSGI_APPLICATION = 'HealthCaretsuppliestracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'backendapi',
+        'USER': 'moringa',
+    'PASSWORD':'7166',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
