14649455660e75b0ca86b30173d37c998bded97f
Koech-code <nixon.koech@moringaschool.com>
2021-10-08 23:34:29 +0300

    initial commits

--- HealthCaretsuppliestracker/settings.py
+++ HealthCaretsuppliestracker/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'healthCaresupplies'
+    'healthCaretsuppliestracker',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
