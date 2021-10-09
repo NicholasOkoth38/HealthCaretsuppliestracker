1f3ba68893bb099d8ace629b98eb90a4fd8896f2
Koech-code <nixon.koech@moringaschool.com>
2021-10-09 15:16:51 +0300

    create a url endpoint for email verification

--- healthCaretsuppliestracker/urls.py
+++ healthCaretsuppliestracker/urls.py
@@ -3,7 +3,5 @@ from . views import RegisterView, verifyEmail
 
 urlpatterns=[
     url('register/', RegisterView.as_view(), name='register'), 
-    url('email-verify/', verifyEmail.as_view(), name='verify-email'), 
-
-     
+    url('email-verify/', verifyEmail.as_view(), name='verify-email'),   
 ]
